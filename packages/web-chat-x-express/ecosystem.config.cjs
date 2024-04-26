25eda83e87b25fecb8950c8d2ad3ab88ad6fe986
杜晓浩 <1923911906@qq.com>
2024-04-26 23:28:30 +0800

    perf: pm2 config

--- packages/web-chat-x-express/ecosystem.config.cjs
+++ packages/web-chat-x-express/ecosystem.config.cjs
@@ -3,9 +3,9 @@ module.exports = {
     {
       name: "web-chat-x-express", // app name
       script: "./index.js", // 启动执行文件
+      node_args: "--harmony",
       watch: true,
-      exec_mode: "cluster",
-      instances: "max",
+      exec_mode: "fork",
       watch_delay: 1000, // 文件变化后，延迟重启时间
     },
   ],
